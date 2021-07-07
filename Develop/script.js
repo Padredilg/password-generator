a36f467dc397314769d9ba405c58353f088597de
Luiz P <padredilg@knights.ucf.edu>
2021-07-06 22:00:33 -0400

    Created 4 arrays to hold each set of symbols

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,10 @@
 
 var generatePassword = function(){
   //create arrays and prompts to sort the final Password
+  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var upperCase = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
+  var lowerCase = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+  var specialSymbols = ['!','@','#','$','%','^','&','*','(',')'];
 }
 
 // Get references to the #generate element
