d816ab843c8b80cb6d897834cd909bfe86eb59ba
Luiz P <padredilg@knights.ucf.edu>
2021-07-06 22:11:42 -0400

    created string for passwordd, added logic to input chars, and return string from function

--- Develop/script.js
+++ Develop/script.js
@@ -9,35 +9,43 @@ var generatePassword = function(){
 
   var concatenatedArray = [];
 
-    //create questions//asking lenght
-    var lengthQuestion = window.prompt("How many characters do you want your password to have?");
-    while((isNaN(lengthQuestion)) || lengthQuestion<8 || lengthQuestion>128){//you can add while loops for each condition inside this while loop so to have different messages for each.
-      window.alert("Your input must contain only numbers and must be no less than 8 characters long or 128 characters long");
-      lengthQuestion = window.prompt("How many characters do you want your password to have?");
-    }//now we know how long finalPassword will be.
+  //create questions//asking lenght
+  var lengthQuestion = window.prompt("How many characters do you want your password to have?");
+  while((isNaN(lengthQuestion)) || lengthQuestion<8 || lengthQuestion>128){//you can add while loops for each condition inside this while loop so to have different messages for each.
+  window.alert("Your input must contain only numbers and must be no less than 8 characters long or 128 characters long");
+    lengthQuestion = window.prompt("How many characters do you want your password to have?");
+  }//now we know how long finalPassword will be.
   
-    //asking for upper case
-    var upCaseQuestion = window.confirm("Would you like your password to include Upper Case Letters?");
-    if(upCaseQuestion){
-      concatenatedArray = concatenatedArray.concat(upperCase);
-    }
+  //asking for upper case
+  var upCaseQuestion = window.confirm("Would you like your password to include Upper Case Letters?");
+  if(upCaseQuestion){
+    concatenatedArray = concatenatedArray.concat(upperCase);
+  }
   
-    //asking for lower case
-    var lowCaseQuestion = window.confirm("Would you like your password to include Lower Case Letters?");
-    if(lowCaseQuestion){
-      concatenatedArray = concatenatedArray.concat(lowerCase);
-    }
+  //asking for lower case
+  var lowCaseQuestion = window.confirm("Would you like your password to include Lower Case Letters?");
+  if(lowCaseQuestion){
+    concatenatedArray = concatenatedArray.concat(lowerCase);
+  }
   
-    //asking for numbers
-    var numericQuestion = window.confirm("Would you like your password to include Numbers?");
-    if(numericQuestion){
-      concatenatedArray = concatenatedArray.concat(numbers);
-    }
+  //asking for numbers
+  var numericQuestion = window.confirm("Would you like your password to include Numbers?");
+  if(numericQuestion){
+    concatenatedArray = concatenatedArray.concat(numbers);
+  }
   
-    var specialSymbolQuestion = window.confirm("Would you like your password to include special symbols?");
-    if(specialSymbolQuestion){
-      concatenatedArray = concatenatedArray.concat(specialSymbols);
-    }
+  var specialSymbolQuestion = window.confirm("Would you like your password to include special symbols?");
+  if(specialSymbolQuestion){
+    concatenatedArray = concatenatedArray.concat(specialSymbols);
+  }
+
+  var finalPassword = "";
+
+  for(var i=0; i<lengthQuestion; i++){
+    finalPassword += concatenatedArray[Math.floor(Math.random()*concatenatedArray.length)];
+  }
+
+  return finalPassword;
 }
 
 // Get references to the #generate element
