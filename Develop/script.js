59221b5d3eaa1007a75d25a0a91d0b5fa64624cb
Luiz P <padredilg@knights.ucf.edu>
2021-07-06 22:59:08 -0400

    added more symbols to specialSymbols set

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,8 @@ var generatePassword = function(){
   var numbers = [0,1,2,3,4,5,6,7,8,9];
   var upperCase = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
   var lowerCase = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
-  var specialSymbols = ['!','@','#','$','%','^','&','*','(',')'];//add more symbols
-
+  var specialSymbols = ['!','@','#','$','%','^','&','*','(',')', '*', '+', '-', ',', '/', '.', ':', ';', '?', '_', '~'];
+  
   var concatenatedArray = [];
 
   //create questions//asking lenght
